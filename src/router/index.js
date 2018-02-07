1c963118798546d243abf770348049feb5de5cf6
Oskar Yildiz <yildizoskar@gmail.com>
2018-02-07 19:34:33 +0100

    added FillOutInfo screen to appropriate router

--- src/router/index.js
+++ src/router/index.js
@@ -73,7 +73,7 @@ const SignedInNav = TabNavigator(
     tabBarComponent: TabBarBottom,
     tabBarPosition: 'bottom',
     animationEnabled: false,
-    swipeEnabled: false
+    swipeEnabled: true
   }
 );
 
