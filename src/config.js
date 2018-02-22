8fd75c9245f7398900e7b3d496750d317e1e2a53
Oskar Yildiz <yildizoskar@gmail.com>
2018-02-22 19:37:27 +0100

    apiUrl change for local dev

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 const config = {
-  apiUrl: `http://ssis.nu:4444/api`
-  // process.env.NODE_ENV === 'production'
-  //   ? `http://ssis.nu:4444/api`
-  //   : `http://192.168.1.246:4000/api`
+  // apiUrl: `http://ssis.nu:4444/api`
+  // apiUrl:
+  //   process.env.NODE_ENV === 'production'
+  //     ? `http://ssis.nu:4444/api`
+  //     : `http://192.168.1.246:4000/api`
+  apiUrl: `http://10.0.0.138:4000/api`
 };
 
 export default config;
