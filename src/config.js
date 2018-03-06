40bdf81ad47952a0386a800056917e9f8e175c80
Oskar Yildiz <yildizoskar@gmail.com>
2018-03-06 14:39:55 +0100

    changes to apiurl because change of local private ipaddress - this config needs to be ignored in the future

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const config = {
   //   process.env.NODE_ENV === 'production'
   //     ? `http://ssis.nu:4444/api`
   //     : `http://192.168.1.246:4000/api`
-  apiUrl: `http://192.168.1.246:4000/api`
+  apiUrl: `http://192.168.50.242:4000/api`
 };
 
 export default config;
