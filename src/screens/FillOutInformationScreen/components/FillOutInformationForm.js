c4e7dd590581a0075e98d299bfbb04ceb9a0ffac
Oskar Yildiz <yildizoskar@gmail.com>
2018-04-18 21:57:17 +0200

    Added picker for selecting classes

--- src/screens/FillOutInformationScreen/components/FillOutInformationForm.js
+++ src/screens/FillOutInformationScreen/components/FillOutInformationForm.js
@@ -1,22 +1,38 @@
 import { reduxForm, Field } from 'redux-form';
-import { View, Button } from 'react-native';
+import { View, Button, Picker } from 'react-native';
 import React from 'react';
 
 import Input from '../../../components/Input';
+import ClassPicker from './ClassPicker';
+import classesData from './classes.json';
 
 const FillOutInformationForm = ({
   handleSubmit,
   submitting,
   invalid,
   error,
-  onSubmit
+  onSubmit,
 }) => (
   <View>
     <Field component={Input} name="name" placeholder="Namn" label="Namn" />
     <Field component={Input} name="class" placeholder="Klass" label="Klass" />
+    <Field name="class" component={ClassPicker}>
+    {renderClasses()}
+    </Field>
     <Button onPress={handleSubmit(onSubmit)} title="Klar!" />
   </View>
 );
+
+const renderClasses = () => {
+  console.log(classesData);
+  const pickerItems = [];
+  for(let i = 0; i < classesData.classes.length; i++) {
+    console.log(classesData.classes[i]);
+    pickerItems.push(<Picker.Item key={classesData.classes[i]} label={classesData.classes[i]} value={classesData.classes[i]} />);
+  }
+  return pickerItems;
+}
+
 
 export default reduxForm({ form: 'fillOutInformation' })(
   FillOutInformationForm
