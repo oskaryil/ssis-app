167b8b7be979dc66af4e4da0515d0982ce522c6a
Oskar Yildiz <yildizoskar@gmail.com>
2018-06-03 14:05:44 +0200

    Redesigned lunch menu screen to match the invision prototype and also switched to an example lunch menu for the time being

--- src/screens/LunchMenuScreen/index.js
+++ src/screens/LunchMenuScreen/index.js
@@ -7,36 +7,36 @@ import commonStyles from '../../styles/common.styles';
 import lunchMenuStyles from './lunchMenu.styles';
 import { fetchLunchMenu } from '../../redux/lunch/actions';
 
-// const lunchMenu = {
-//   Måndag: [
-//     'Köttbullar med potatis och lingon',
-//     'Fylld paprika på Sojafärs',
-//     'Bakad lax med jasminris'
-//   ],
-//   Tisdag: [
-//     'Grillad kycklingbröst med chimichurri och örtris',
-//     'Pasta gryta med fetaost och fruktig olivolja G/M/Ä',
-//     'Salviabakad kummel med vitvinsås, apelsinbakad tomat och ris M'
-//   ],
-//   Onsdag: [
-//     'Nattbakad fläskkaree med senaps och rotfruktssallad samt kräftigbuljong',
-//     'Indisk kikärtsgryta chana masala style med jasminris',
-//     'Rostad Torsk samt citronkokt rödbetor och rökt potatis pure M'
-//   ],
-//   Torsdag: [
-//     'Lammgryta med purjolök, fetaost och basmatiris M',
-//     'Fransk löksoppa med parmesancrutonger Ä/M Ärtsoppa, vanilj och honungsgrädde, sylt och pankakor Ä/G/M',
-//     'Ingefära och soyabakad kummel samt broccoli och ris'
-//   ],
-//   Fredag: [
-//     'Bratwurst med surkål, stekt potatis och senapssky M',
-//     'Panko panerade Svampbiffar, rostad potatis och tzatziki G/M',
-//     'Rimmad lax med dillstuvad potatis M'
-//   ]
-// };
+// Example lunch menu
+const lunchMenu = {
+  Måndag: [
+    'Köttbullar med potatis och lingon',
+    'Fylld paprika på Sojafärs',
+    'Bakad lax med jasminris'
+  ],
+  Tisdag: [
+    'Grillad kycklingbröst med chimichurri och örtris',
+    'Pasta gryta med fetaost och fruktig olivolja G/M/Ä',
+    'Salviabakad kummel med vitvinsås, apelsinbakad tomat och ris M'
+  ],
+  Onsdag: [
+    'Nattbakad fläskkaree med senaps och rotfruktssallad samt kräftigbuljong',
+    'Indisk kikärtsgryta chana masala style med jasminris',
+    'Rostad Torsk samt citronkokt rödbetor och rökt potatis pure M'
+  ],
+  Torsdag: [
+    'Lammgryta med purjolök, fetaost och basmatiris M',
+    'Fransk löksoppa med parmesancrutonger Ä/M Ärtsoppa, vanilj och honungsgrädde, sylt och pankakor Ä/G/M',
+    'Ingefära och soyabakad kummel samt broccoli och ris'
+  ],
+  Fredag: [
+    'Bratwurst med surkål, stekt potatis och senapssky M',
+    'Panko panerade Svampbiffar, rostad potatis och tzatziki G/M',
+    'Rimmad lax med dillstuvad potatis M'
+  ]
+};
 Date.prototype.getWeek = function() {
-  var onejan = new Date(this.getFullYear(), 0, 1);
-  return Math.ceil(((this - onejan) / 86400000 + onejan.getDay() + 1) / 7);
+  var onejan = new Date(this.getFullYear(), 0, 1); return Math.ceil(((this - onejan) / 86400000 + onejan.getDay() + 1) / 7);
 };
 
 class LunchMenuScreen extends Component {
@@ -48,17 +48,24 @@ class LunchMenuScreen extends Component {
 
   renderLunch = () => {
     const toRender = [];
-    for (let day in this.props.lunchMenu) {
+    const options = ['Kött', 'Fisk', 'Veggo'];
+    // Replace lunchMenu with this.props.lunchMenu
+    for (let day in lunchMenu) {
+      let i = -1;
       toRender.push(
-        <Card style={{ padding: '3%', width: '98%' }} key={day}>
+        <Card style={{ padding: '3%', width: '98%', ...commonStyles.card }} key={day}>
           <Text style={lunchMenuStyles.dayText} key={day}>
             {day}
           </Text>
-          {this.props.lunchMenu[day].map(dish => (
-            <Text style={lunchMenuStyles.dishText} key={dish}>
-              {dish}
-            </Text>
-          ))}
+          {/* Replace lunchmenu with  this.props.lunchMenu */}
+          {lunchMenu[day].map(dish => {
+            i += 1;
+            return (
+              <Text style={lunchMenuStyles.dishText} key={dish}>
+                <Text style={{fontWeight: 'bold'}}>{options[i]}</Text> {dish}
+              </Text>
+            );
+          })}
         </Card>
       );
     }
@@ -68,10 +75,11 @@ class LunchMenuScreen extends Component {
   render() {
     return (
       <Screen styleName="paper">
-      <NavigationBar title={`Lunch meny v. ${new Date().getWeek()}`} />
+      <NavigationBar styleName="inline" title={`Lunch meny v. ${new Date().getWeek()}`} />
       <ScrollView
         contentContainerStyle={lunchMenuStyles.contentContainer}
-        styleName="paper v-scroll">
+        style={lunchMenuStyles.container}
+        >
           <View style={lunchMenuStyles.lunchMenuBox}>
             {/* <Text style={lunchMenuStyles.dayText}>Måndag</Text>
             <Text style={lunchMenuStyles.dayText}>Tisdag</Text> */}
