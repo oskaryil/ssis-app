72d7d7a893006496a7c081a5a9b21d29419bf6cb
Oskar Yildiz <yildizoskar@gmail.com>
2018-08-05 01:52:05 +0200

    UI progression

--- src/screens/TodoListScreen/index.js
+++ src/screens/TodoListScreen/index.js
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Text } from "react-native";
-import { NavigationBar, Screen, ScrollView, Button } from "@shoutem/ui";
+import { Text, Modal } from "react-native";
+import {
+  NavigationBar,
+  Screen,
+  ScrollView,
+  Button,
+  View,
+  Heading
+} from "@shoutem/ui";
 import { Ionicons } from "@expo/vector-icons";
+import Input from "../../components/Input";
+import CreateTodoForm from "./components/CreateTodoForm";
 
-// import scheduleStyles from './schedule.styles';
+import modalStyles from "./styles/modal.styles";
 import commonStyles from "../../styles/common.styles";
 // import { getFullSchedule } from '../../redux/schedule/actions';
 // import ClassCard from './components/ClassCard';
 
 class TodoListScreen extends Component {
   static navigationOptions = {
-    tabBarLabel: "Att göra"
+    tabBarLabel: "Att göra",
+    header: null
   };
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      addTodoModalVisible: false
+    };
   }
 
   async componentWillMount() {}
@@ -27,11 +41,35 @@ class TodoListScreen extends Component {
           title="Att göra"
           styleName="inline"
           rightComponent={
-            <Button onPress={() => console.log("hello")}>
+            <Button
+              onPress={() => this.setState({ addTodoModalVisible: true })}
+            >
               <Ionicons name="ios-add" size={36} />
             </Button>
           }
         />
+        <Modal
+          visible={this.state.addTodoModalVisible}
+          animationType="slide"
+          transparent={false}
+          onRequestClose={() => {
+            alert("Modal has been closed.");
+          }}
+        >
+          <View style={modalStyles.container}>
+            <Button
+              style={modalStyles.closeBtn}
+              onPress={() => this.setState({ addTodoModalVisible: false })}
+            >
+              <Ionicons name="ios-close" size={46} />
+            </Button>
+            <Heading style={modalStyles.heading}>Lägg till att-göra</Heading>
+            <CreateTodoForm
+              style={modalStyles.form}
+              onSubmit={values => console.log(values)}
+            />
+          </View>
+        </Modal>
       </Screen>
     );
   }
---
+++ src/screens/TodoListScreen/styles/modal.styles.js
@@ -0,0 +1,17 @@
+export default {
+  container: {
+    flex: 1,
+    paddingTop: 30
+  },
+  closeBtn: {
+    alignSelf: "flex-end",
+    paddingRight: 10
+  },
+  heading: {
+    alignSelf: "center",
+    marginBottom: 24
+  },
+  form: {
+    marginTop: 20
+  }
+};
