d7ca0f0afd9b37f5d395e0b92e9c2c49fd0d3123
Oskar Yildiz <yildizoskar@gmail.com>
2018-08-05 15:50:08 +0200

    Enabled swipe on each screen

--- src/screens/LunchMenuScreen/index.js
+++ src/screens/LunchMenuScreen/index.js
@@ -1,46 +1,49 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Alert } from 'react-native';
-import { connect } from 'react-redux';
-import { ScrollView, Card, Spinner, NavigationBar, Screen } from '@shoutem/ui';
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
+import { connect } from "react-redux";
+import { ScrollView, Card, Spinner, NavigationBar, Screen } from "@shoutem/ui";
 
-import commonStyles from '../../styles/common.styles';
-import lunchMenuStyles from './lunchMenu.styles';
-import { fetchLunchMenu } from '../../redux/lunch/actions';
+import commonStyles from "../../styles/common.styles";
+import lunchMenuStyles from "./lunchMenu.styles";
+import { fetchLunchMenu } from "../../redux/lunch/actions";
 
 // Example lunch menu
 const lunchMenu = {
   Måndag: [
-    'Köttbullar med potatis och lingon',
-    'Fylld paprika på Sojafärs',
-    'Bakad lax med jasminris'
+    "Köttbullar med potatis och lingon",
+    "Fylld paprika på Sojafärs",
+    "Bakad lax med jasminris"
   ],
   Tisdag: [
-    'Grillad kycklingbröst med chimichurri och örtris',
-    'Pasta gryta med fetaost och fruktig olivolja G/M/Ä',
-    'Salviabakad kummel med vitvinsås, apelsinbakad tomat och ris M'
+    "Grillad kycklingbröst med chimichurri och örtris",
+    "Pasta gryta med fetaost och fruktig olivolja G/M/Ä",
+    "Salviabakad kummel med vitvinsås, apelsinbakad tomat och ris M"
   ],
   Onsdag: [
-    'Nattbakad fläskkaree med senaps och rotfruktssallad samt kräftigbuljong',
-    'Indisk kikärtsgryta chana masala style med jasminris',
-    'Rostad Torsk samt citronkokt rödbetor och rökt potatis pure M'
+    "Nattbakad fläskkaree med senaps och rotfruktssallad samt kräftigbuljong",
+    "Indisk kikärtsgryta chana masala style med jasminris",
+    "Rostad Torsk samt citronkokt rödbetor och rökt potatis pure M"
   ],
   Torsdag: [
-    'Lammgryta med purjolök, fetaost och basmatiris M',
-    'Fransk löksoppa med parmesancrutonger Ä/M Ärtsoppa, vanilj och honungsgrädde, sylt och pankakor Ä/G/M',
-    'Ingefära och soyabakad kummel samt broccoli och ris'
+    "Lammgryta med purjolök, fetaost och basmatiris M",
+    "Fransk löksoppa med parmesancrutonger Ä/M Ärtsoppa, vanilj och honungsgrädde, sylt och pankakor Ä/G/M",
+    "Ingefära och soyabakad kummel samt broccoli och ris"
   ],
   Fredag: [
-    'Bratwurst med surkål, stekt potatis och senapssky M',
-    'Panko panerade Svampbiffar, rostad potatis och tzatziki G/M',
-    'Rimmad lax med dillstuvad potatis M'
+    "Bratwurst med surkål, stekt potatis och senapssky M",
+    "Panko panerade Svampbiffar, rostad potatis och tzatziki G/M",
+    "Rimmad lax med dillstuvad potatis M"
   ]
 };
 Date.prototype.getWeek = function() {
-  var onejan = new Date(this.getFullYear(), 0, 1); return Math.ceil(((this - onejan) / 86400000 + onejan.getDay() + 1) / 7);
+  var onejan = new Date(this.getFullYear(), 0, 1);
+  return Math.ceil(((this - onejan) / 86400000 + onejan.getDay() + 1) / 7);
 };
 
 class LunchMenuScreen extends Component {
-  static navigationOptions = {};
+  static navigationOptions = {
+    swipeEnabled: true
+  };
 
   async componentDidMount() {
     await this.props.fetchLunchMenu();
@@ -48,12 +51,15 @@ class LunchMenuScreen extends Component {
 
   renderLunch = () => {
     const toRender = [];
-    const options = ['Kött', 'Fisk', 'Veggo'];
+    const options = ["Kött", "Fisk", "Veggo"];
     // Replace lunchMenu with this.props.lunchMenu
     for (let day in lunchMenu) {
       let i = -1;
       toRender.push(
-        <Card style={{ padding: '3%', width: '98%', ...commonStyles.card }} key={day}>
+        <Card
+          style={{ padding: "3%", width: "98%", ...commonStyles.card }}
+          key={day}
+        >
           <Text style={lunchMenuStyles.dayText} key={day}>
             {day}
           </Text>
@@ -62,7 +68,7 @@ class LunchMenuScreen extends Component {
             i += 1;
             return (
               <Text style={lunchMenuStyles.dishText} key={dish}>
-                <Text style={{fontWeight: 'bold'}}>{options[i]}</Text> {dish}
+                <Text style={{ fontWeight: "bold" }}>{options[i]}</Text> {dish}
               </Text>
             );
           })}
@@ -75,18 +81,20 @@ class LunchMenuScreen extends Component {
   render() {
     return (
       <Screen styleName="paper">
-      <NavigationBar styleName="inline" title={`Lunch meny v. ${new Date().getWeek()}`} />
-      <ScrollView
-        contentContainerStyle={lunchMenuStyles.contentContainer}
-        style={lunchMenuStyles.container}
+        <NavigationBar
+          styleName="inline"
+          title={`Lunch meny v. ${new Date().getWeek()}`}
+        />
+        <ScrollView
+          contentContainerStyle={lunchMenuStyles.contentContainer}
+          style={lunchMenuStyles.container}
         >
           <View style={lunchMenuStyles.lunchMenuBox}>
             {/* <Text style={lunchMenuStyles.dayText}>Måndag</Text>
             <Text style={lunchMenuStyles.dayText}>Tisdag</Text> */}
             {this.props.lunchMenu ? this.renderLunch() : <Spinner />}
           </View>
-      </ScrollView>
-
+        </ScrollView>
       </Screen>
     );
   }
@@ -96,4 +104,7 @@ function mapStateToProps(state) {
   return { auth: state.auth, lunchMenu: state.lunch };
 }
 
-export default connect(mapStateToProps, { fetchLunchMenu })(LunchMenuScreen);
+export default connect(
+  mapStateToProps,
+  { fetchLunchMenu }
+)(LunchMenuScreen);
