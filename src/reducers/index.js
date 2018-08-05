71c9cc6c9c8fcb4c9b58667976ede72723ae5e03
Oskar Yildiz <yildizoskar@gmail.com>
2018-08-05 15:49:24 +0200

    Added TodoList reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,15 @@
-import { combineReducers } from 'redux';
-import { persistCombineReducers } from 'redux-persist';
-import { AsyncStorage as storage } from 'react-native'; // default: localStorage if web, AsyncStorage if react-native
-import { reducer as formReducer } from 'redux-form';
+import { combineReducers } from "redux";
+import { persistCombineReducers } from "redux-persist";
+import { AsyncStorage as storage } from "react-native"; // default: localStorage if web, AsyncStorage if react-native
+import { reducer as formReducer } from "redux-form";
 
-import AuthReducer from '../redux/auth/reducer';
-import LunchReducer from '../redux/lunch/reducer';
-import ScheduleReducer from '../redux/schedule/reducer';
+import AuthReducer from "../redux/auth/reducer";
+import LunchReducer from "../redux/lunch/reducer";
+import ScheduleReducer from "../redux/schedule/reducer";
+import TodoListReducer from "../redux/todoList/reducer";
 
 const config = {
-  key: 'root',
+  key: "root",
   storage
 };
 
@@ -16,7 +17,8 @@ const reducers = persistCombineReducers(config, {
   auth: AuthReducer,
   schedule: ScheduleReducer,
   lunch: LunchReducer,
-  form: formReducer
+  form: formReducer,
+  todoList: TodoListReducer
 });
 
 export default reducers;
