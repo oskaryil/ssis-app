e3e136d41dc6ac65105c9f86e091750703527f47
Oskar Yildiz <yildizoskar@gmail.com>
2018-08-06 16:56:55 +0200

    Created action that fetches user todos

--- src/redux/todoList/actions.js
+++ src/redux/todoList/actions.js
@@ -1,8 +1,33 @@
-import { CREATE_TODO, CREATE_TODO_SUCCESS, CREATE_TODO_FAIL } from "./types";
+import axios from "axios";
+
+import {
+  FETCH_TODOS,
+  FETCH_TODOS_SUCCESS,
+  FETCH_TODOS_FAIL,
+  CREATE_TODO,
+  CREATE_TODO_SUCCESS,
+  CREATE_TODO_FAIL
+} from "./types";
+
+const fetchTodos = () => async (dispatch, getState) => {
+  dispatch({ type: FETCH_TODOS });
+  try {
+    const { data } = await axios({
+      method: "get",
+      url: `/todo`,
+      headers: {
+        Authorization: `${getState().auth.user.token}`
+      }
+    });
+    dispatch({ type: FETCH_TODOS_SUCCESS, todos: data });
+  } catch (err) {
+    dispatch({ type: FETCH_TODOS_FAIL, error: err });
+  }
+};
 
 const createTodo = values => dispatch => {
   dispatch({ type: CREATE_TODO });
   dispatch({ type: CREATE_TODO_SUCCESS, todo: values });
 };
 
-export { createTodo };
+export { fetchTodos, createTodo };
