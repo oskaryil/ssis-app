dfa2ea91b3c997bf182dfb57c8a53c98d01735ce
Oskar Yildiz <yildizoskar@gmail.com>
2018-08-06 16:56:23 +0200

    Added types for fetching todos

--- src/redux/todoList/types.js
+++ src/redux/todoList/types.js
@@ -1,3 +1,6 @@
 export const CREATE_TODO = "create_todo";
 export const CREATE_TODO_SUCCESS = "create_todo_success";
 export const CREATE_TODO_FAIL = "create_todo_fail";
+export const FETCH_TODOS = "fetch_todos";
+export const FETCH_TODOS_SUCCESS = "fetch_todos_success";
+export const FETCH_TODOS_FAIL = "fetch_todos_fail";
